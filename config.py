c63833546c3aa4bbcd1371e2507562dc9a307c09
omarsayedali <85970161+omarsayedali@users.noreply.github.com>
2025-12-17 15:45:18 +0200

    Added dockerignore to fix DB connection

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv() # This is fine, but Railway variables will now take priority
 
 class Config:
-    # This secret key is for security (cookies/sessions)
-    SECRET_KEY = 'dev-secret-key'
+    # It must check os.environ first!
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     
-    # DATABASE CONFIGURATION
-    # If we are on Railway, use their DB.
-    # If we are on your laptop, use a local file named 'site.db'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    
+    # Fix for the "postgres://" vs "postgresql://" issue on Railway
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
